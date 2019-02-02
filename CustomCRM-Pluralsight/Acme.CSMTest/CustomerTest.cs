15a02ae1bdd13d424460f913cb53c84ba43e5826
Guy Whorley <guy.whorley@cherwell.com>
2019-02-01 18:16:47 -0800

    Add customer, add unit tests.

---
+++ CustomCRM-Pluralsight/Acme.CSMTest/CustomerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Acme.CMS.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.CSMTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        [TestCategory("Valid")]
+        public void FullName_IsValid()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Testy",
+                LastName = "McTesterson"
+            };
+
+            // Act
+            string expected = "Testy McTesterson";
+
+            // Assert
+            Assert.AreEqual(expected, customer.FullName);
+        }
+
+        [TestMethod]
+        [TestCategory("Valid")]
+        public void FullName_FirstNameEmpty()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                LastName = "McTesterson"
+            };
+
+            // Act
+            string expected = "McTesterson";
+
+            // Assert
+            Assert.AreEqual(expected, customer.FullName);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Valid")]
+        public void FullName_LastNameEmpty()
+        {
+            // Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Testy",
+            };
+
+            // Act
+            string expected = "Testy";
+
+            // Assert
+            Assert.AreEqual(expected, customer.FullName);
+
+        }
+    }
+}
---
+++ CustomCRM-Pluralsight/CustomCRM-Pluralsight/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace Acme.CMS.Entities
+{
+    public class Customer
+    {
+        /// <summary>
+        /// First name.
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name.
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Full name.
+        /// </summary>
+        public string FullName => $"{FirstName} {LastName}".Trim();
+
+        /// <summary>
+        /// Email address.
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// CustomerId.
+        /// </summary>
+        public int CustomerId { get; private set; }
+
+
+    }
+}
