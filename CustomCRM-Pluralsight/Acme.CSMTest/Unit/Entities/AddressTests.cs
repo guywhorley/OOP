c83d16a56cc5ba0ccf55a2cbbab0ca73c70f3f14
Guy Whorley <guy.whorley@cherwell.com>
2019-02-02 15:08:15 -0800

    Add Address unit test.

---
+++ CustomCRM-Pluralsight/Acme.CSMTest/Unit/Entities/AddressTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Acme.CMS.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.CSMTest.Unit.Entities
+{
+    [TestClass]
+    public class Address_Should
+    {
+        private Address m_address;
+        private readonly int m_testId = 1;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            m_address = new Address(m_testId);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            m_address = null;
+        }
+        [TestMethod]
+        public void Have_Been_Created_With_Assigned_AddressId()
+        {
+            Assert.AreEqual(m_testId, m_address.AddressId);
+        }
+    }
+}
--- CustomCRM-Pluralsight/CustomCRM-Pluralsight/Entities/Address.cs
+++ CustomCRM-Pluralsight/CustomCRM-Pluralsight/Entities/Address.cs
@@ -11,31 +11,60 @@ namespace Acme.CMS.Entities
     /// </summary>
     public class Address
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public Address()
         {
         }
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="addressId"></param>
         public Address(int addressId)
         {
             this.AddressId = addressId;
         }
 
+        /// <summary>
+        /// Address Id.
+        /// </summary>
         public int AddressId { get; private set; }
 
+        /// <summary>
+        /// Address Type.
+        /// </summary>
         public int AddressType { get; set; }
 
+        /// <summary>
+        /// Address StreetLine 1.
+        /// </summary>
         public string StreetLine1 { get; set; }
 
+        /// <summary>
+        /// Address StreetLine 2.
+        /// </summary>
         public string  StreetLine2 { get; set; }
 
+        /// <summary>
+        /// Address City.
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// Address State.
+        /// </summary>
         public string State { get; set; }
 
+        /// <summary>
+        /// Address zip code.
+        /// </summary>
         public string  PostalCode { get; set; }
 
+        /// <summary>
+        /// Address Country.
+        /// </summary>
         public string Country { get; set; }
-
-        public int ZipCode { get; set; }
     }
 }
