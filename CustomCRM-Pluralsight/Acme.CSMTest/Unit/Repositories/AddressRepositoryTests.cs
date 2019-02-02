73925bf2e38965d9c430f708485eb109789b78b1
Guy Whorley <guy.whorley@cherwell.com>
2019-02-02 15:58:08 -0800

    Add AddressRepository unit tests and Repository.

---
+++ CustomCRM-Pluralsight/Acme.CSMTest/Unit/Repositories/AddressRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Acme.CMS.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.CSMTest.Unit.Repositories
+{
+    [TestClass]
+    public class AddressRepository_Should
+    {
+        private AddressRepository m_addressRepository;
+        private readonly int m_testId = 1;
+        private readonly string m_City = "Testville";
+        private readonly int m_count = 3;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            m_addressRepository = new AddressRepository();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            m_addressRepository = null;
+        }
+
+        [TestMethod]
+        public void Return_True_On_Save1()
+        {
+            Assert.IsTrue(m_addressRepository.Save());
+        }
+
+        [TestMethod]
+        public void Return_Address_By_Id()
+        {
+            Assert.AreEqual(m_City, m_addressRepository.Retrieve(m_testId).City);
+        }
+
+        [TestMethod]
+        public void Return_All_Addresses()
+        {
+            Assert.AreEqual(m_count, m_addressRepository.Retrieve().Count);
+        }
+    }
+}
---
+++ CustomCRM-Pluralsight/CustomCRM-Pluralsight/Repositories/AddressRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.CMS.Entities;
+
+namespace Acme.CMS.Repositories
+{
+    public class AddressRepository
+    {
+        /// <summary>
+        /// Save the Address.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save() => true;
+
+        /// <summary>
+        /// Retrieve one customer.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Address Retrieve(int addressId)
+        {
+            var address = new Address(addressId);
+
+            if (addressId == 1)
+            {
+                address.StreetLine1 = "123 4th Ave NE";
+                address.City = "Testville";
+                address.State = "Test";
+                address.Country = "USA";
+                address.PostalCode = "55555";
+            }
+            return address;
+        }
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        /// <returns></returns>
+        public List<Address> Retrieve()
+        {
+            return new List<Address>()
+            {
+                new Address(123),
+                new Address(456),
+                new Address(789)
+            };
+        }
+    }
+}
